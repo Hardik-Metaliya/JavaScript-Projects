04621f8dc49a3b23e0747e9012c2e4b7e5288d45
Hardik-Metaliya <hardikmetaliya6121@gmail.com>
2023-05-31 14:18:56 +0530

    complete
    to-do
    make it movable by holding

--- 1 - Dragable-Model/script.js
+++ 1 - Dragable-Model/script.js
@@ -1,1 +1,14 @@
 console.log("JS"); // just to test remove it after use
+const button = document.querySelector(".show-model");
+const overlay = document.querySelector(".sheet-overlay");
+const main = document.querySelector(".content");
+button.addEventListener("click", () => {
+  overlay.classList.toggle("hidden");
+  main.classList.toggle("hidden");
+  button.style.zIndex = "0";
+});
+overlay.addEventListener("click", () => {
+  overlay.classList.toggle("hidden");
+  main.classList.toggle("hidden");
+  button.style.zIndex = "1";
+});
